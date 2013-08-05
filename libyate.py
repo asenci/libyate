59ac5434704e69876d4773851f34c5a4520c2d86
Andre Sencioles <asenci@gmail.com>
2013-08-05 17:28:14 -0300

    New classes: YateApp, YateScript, YateSocketClient
    YateApp: Base class for Yate external modules
    YateScript: Class prototype for external scripts
    YateSocketClient: Stub for socket connecting applications

--- libyate.py
+++ libyate.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import datetime
+import logging
 import select
 import sys
 
+
+# Keywords definition
 KW_CONNECT = '%%>connect'
 KW_ERROR = 'Error in'
 KW_INSTALL = '%%>install'
@@ -20,41 +23,176 @@ KW_WATCH = '%%>watch'
 KW_WATCHREPLY = '%%<watch'
 
 KW_MAP = {
-    '%%>message': ['method', 'id', 'timestamp', 'name', 'retval', 'extras'],
-    '%%<message': ['method', 'id', 'processed', 'name', 'retval', 'extras'],
-    '%%>install': ['method', 'priority', 'name', 'filtername', 'filtervalue'],
-    '%%<install': ['method', 'priority', 'name', 'success'],
-    '%%>uninstall': ['method', 'name'],
-    '%%<uninstall': ['method', 'priority', 'name', 'success'],
-    'Error in': ['command'],
+    KW_CONNECT: ['role', 'id', 'type'],
+    KW_ERROR: ['command'],
+    KW_INSTALL: ['method', 'priority', 'name', 'filtername', 'filtervalue'],
+    KW_INSTALLREPLY: ['method', 'priority', 'name', 'success'],
+    KW_MESSAGE: ['method', 'id', 'timestamp', 'name', 'retval', 'extras'],
+    KW_MESSAGEREPLY: ['method', 'id', 'processed', 'name', 'retval', 'extras'],
+    KW_OUTPUT: ['output'],
+    KW_SETLOCAL: ['method', 'name', 'value'],
+    KW_SETLOCALREPLY: ['method', 'name', 'value', 'success'],
+    KW_UNINSTALL: ['method', 'name'],
+    KW_UNINSTALLREPLY: ['method', 'priority', 'name', 'success'],
+    KW_UNWATCH: ['method', 'name'],
+    KW_UNWATCHREPLY: ['method', 'name', 'success'],
+    KW_WATCH: ['method', 'name'],
+    KW_WATCHREPLY: ['method', 'name', 'success'],
 }
 
 
-class Poller:
-    def __init__(self, fd):
-        self.fd = fd
+class YateApp(object):
+    """Base class for Yate applications"""
 
+    def __init__(self, handler_map=None):
+        logging.basicConfig(level=logging.INFO, format='%(message)s')
+        self.logger = logging.getLogger(__name__)
+
+        self.queues = {
+            KW_INSTALL: {},
+            KW_MESSAGE: {},
+            KW_SETLOCAL: {},
+            KW_UNINSTALL: {},
+            KW_UNWATCH: {},
+            KW_WATCH: {},
+        }
+
+        if handler_map:
+            self.handlers = handler_map
+        else:
+            self.handlers = {
+                KW_MESSAGE: self.handle_message,
+                KW_MESSAGEREPLY: self.handle_message_reply,
+            }
+
+    def handle_command(self, line):
+        """Handle incoming commands"""
+        if line.strip():
+            self.logger.debug('Received: {}'.format(line.strip()))
+
+            try:
+                # Try parsing the command
+                cmd = cmd_to_dict(line)
+            except Exception, e:
+                self.logger.error(e.message)
+            else:
+                # Dispatch the command to the handler based on the method
+                try:
+                    handler = self.handlers[cmd['method']]
+                except KeyError:
+                    raise NotImplementedError(
+                        'Method "{}" not implemented'.format(cmd['method']))
+                else:
+                    self.logger.debug(
+                        'Using function "{}" to process the command'
+                        .format(handler.__name__))
+                    handler(cmd)
+
+    def handle_message(self, command):
+        """Simple messages processing stub"""
+
+        # Try to find the message on the queue
+        try:
+            orig = self.queues[command['id']]
+        except KeyError:
+            # New message, store message in the queue
+            self.queues[command['id']] = command
+        else:
+            # Message update, update the message in the queue
+            orig.update(command)
+            command = orig
+
+        # Reply without processing
+        reply = command.copy()
+        reply['method'] = KW_MESSAGEREPLY
+        reply.pop('timestamp', None)
+
+        reply = dict_to_cmd(reply)
+        self.logger.debug('Reply: {}'.format(reply.strip()))
+        self.write(reply)
+
+    def handle_message_reply(self, command):
+        """Simple messages reply processing stub"""
+
+        # Try to find the message on the queue
+        try:
+            orig = self.queues[command['id']]
+        except KeyError:
+            raise KeyError('Invalid message ID on reply: {}'
+                           .format(command['id']))
+
+        # Check if the message processing is done
+        if command['processed']:
+            # Remove message from queue if done
+            del orig
+        else:
+            # Else, update the message parameters
+            orig.update(command)
+
+    def install(self, name, priority, filtername, filtervalue):
+        pass
+
+
+class YateScript(YateApp):
+    """Yate global external module"""
+
+    def __init__(self, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr,
+                 **kwargs):
+        super(YateScript, self).__init__(**kwargs)
+
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+
+        logging.basicConfig(stream=self.stderr)
+
+        # Use epoll if available
         if hasattr(select, 'epoll'):
             self.poller = select.epoll()
-            self.poller.register(self.fd, select.EPOLLIN)
+
+            try:
+                self.poller.register(self.stdin, select.EPOLLIN)
+            except IOError:
+                del self.poller
+        # Else, use poll if available
         elif hasattr(select, 'poll'):
             self.poller = select.poll()
-            self.poller.register(self.fd, select.POLLIN)
+
+            try:
+                self.poller.register(self.stdin, select.POLLIN)
+            except IOError:
+                del self.poller
+
+        # Function to wait for the next command
+        if hasattr(self, 'poller'):
+            self.readline = lambda: (self.poller.poll(), self.stdin.readline())
+        else:
+            # Use select if neither epoll or poll are available
+            self.readline = lambda: (select.select([self.stdin], [], []),
+                                     self.stdin.readline())
 
     def __del__(self):
-        if hasattr(select, 'epoll') or hasattr(select, 'poll'):
-            self.poller.unregister(self.fd)
+        if hasattr(self, 'poller'):
+            # Unregister the file descriptor poller
+            self.poller.unregister(self.stdin)
 
-    def poll(self):
-        if hasattr(select, 'epoll') or hasattr(select, 'poll'):
-            self.poller.poll()
-        else:
-            select.select([self.fd], [], [])
-        return self.fd.readline()
+    def run(self):
+        self.logger.info('Loading script: {}'.format(__file__))
+
+        while True:
+            self.handle_command(self.readline()[1])
+
+    def write(self, command):
+        self.stdout.write(command)
+
+
+class YateSocketClient(YateApp):
+    """TODO: implement a class for IP/socket connecting app"""
+    pass
 
 
 def cmd_to_dict(command):
-    '''Read a command line and parse into a dict'''
+    """Read a command line and parse into a dict"""
 
     # Remove line-feed
     command = command.rstrip('\n')
@@ -87,6 +225,8 @@ def cmd_to_dict(command):
             command[key] = True if command[key] == 'true' else False
         elif key == 'timestamp':
             command[key] = datetime.datetime.fromtimestamp(int(command[key]))
+        elif key == 'output':
+            pass
         elif key == 'method':
             pass
         else:
@@ -96,7 +236,7 @@ def cmd_to_dict(command):
 
 
 def dict_to_cmd(command):
-    '''Parse a dict into a command line'''
+    """Parse a dict into a command line"""
 
     # Get the keys names for this method
     try:
@@ -118,22 +258,25 @@ def dict_to_cmd(command):
             reply.append('true' if command.pop(key, False) else 'false')
         elif key == 'success':
             reply.append('true' if command.pop(key, False) else 'false')
+        elif key == 'output':
+            reply.append(str(command.pop(key, '')))
         elif key == 'extras':
             pass
         elif key == 'method':
             pass
         else:
-            reply.append(str_encode(command.pop(key, '')))
+            reply.append(str_encode(str(command.pop(key, ''))))
 
     # Extra attributes
     for key, value in command.items():
-        reply.append('='.join((str_encode(key), str_encode(value))))
+        reply.append('='.join((str_encode(str(key)),
+                               str_encode(str(value)))))
 
     return ':'.join(reply) + '\n'
 
 
 def lower(char):
-    '''Get the original form form a upcoded char'''
+    """Get the original form form a upcoded char"""
     if char == '%':
         return '%'
     else:
@@ -141,7 +284,7 @@ def lower(char):
 
 
 def str_decode(string):
-    '''Return a escaped string to the original from'''
+    """Return a escaped string to the original from"""
     if '%' in string:
         string, encoded = string.split('%', 1)
 
@@ -154,12 +297,12 @@ def str_decode(string):
 
 
 def str_encode(string):
-    '''Escape a string upcoding the characters'''
+    """Escape a string upcoding the characters"""
     return ''.join(upper(char) for char in list(string))
 
 
 def upper(char):
-    '''Upcode a char'''
+    """Upcode a char"""
     if ord(char) < 32 or char == ':':
         return '%' + chr(64+ord(char))
     elif char == '%':
@@ -168,33 +311,8 @@ def upper(char):
         return char
 
 
-def run():
-    sys.stderr.write('Loading test script: {}\n'.format(__file__))
-    stdin = Poller(sys.stdin)
-
-    while True:
-        line = stdin.poll()
-
-        if line:
-            sys.stderr.write('Received: {}\n'.format(line.strip()))
-            try:
-                cmd = cmd_to_dict(line)
-            except Exception, e:
-                sys.stderr.write(e.message + '\n')
-            else:
-                if cmd['method'] == KW_MESSAGE:
-                    reply = cmd.copy()
-                    del reply['timestamp']
-                    reply.update({
-                        'method': KW_MESSAGEREPLY,
-                        'testing': 'true',
-                    })
-
-                    sys.stderr.write('Reply: {}'.format(dict_to_cmd(reply)))
-
-
 if __name__ == '__main__':
     try:
-        run()
+        YateScript().run()
     except KeyboardInterrupt:
         pass
